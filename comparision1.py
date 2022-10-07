2403b8d0ec3468e00975f62f94389eba558b1dc6
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-07 01:31:51 -0700

    Create comparision1.py

---
+++ comparision1.py
@@ -0,0 +1,8 @@
+num1 = float(input("enter 1st number: "))
+num2 = float(input("enter 2nd number: "))
+if num1>num2:
+  print(num1, "is greater than", num2)
+elif num2>num1:
+  print(num2 ,"is greater than", num1)
+else:
+  print(num1," is equal to", num2)
