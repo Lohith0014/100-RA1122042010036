492f0c8fb186392614e926ae9af98a1d6a087030
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-12 09:38:24 +0530

    Update 13.division.py

--- 13.division.py
+++ 13.division.py
@@ -3,6 +3,7 @@ def div(num1,num2):
   print(percentage)
   return percentage
 
+print("RA2211042010036")
 num1=int(input("enter 1st number: "))
 num2=int(input("enter 2nd number: "))
 div(num1,num2)
