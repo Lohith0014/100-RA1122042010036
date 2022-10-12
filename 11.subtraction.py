49b55d43b551fd278358272724c9c70429de1e1d
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-12 09:37:19 +0530

    Update 11.subtraction.py

--- 11.subtraction.py
+++ 11.subtraction.py
@@ -3,6 +3,7 @@ def sub(num1,num2):
   print(difference)
   return difference
 
+print("RA2211042010036")
 num1=int(input("enter 1st number: "))
 num2=int(input("enter 2nd number: "))
 sub(num1,num2)
