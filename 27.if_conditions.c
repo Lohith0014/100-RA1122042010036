67f7392c99286996567d52d50db1d348171b8b15
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-14 16:35:51 +0530

    Update and rename C13.c to 27.if_conditions.c

---
+++ 27.if_conditions.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+  printf("RA2211042010036\n");
+  int day;
+  printf("Enter day number: ");
+  scanf("%d", &day);
+  
+  if (day==1){
+      printf("monday");
+  }
+  if (day==2){
+      printf("tuesday");
+  }
+  if (day==3){
+      printf("wednesday");
+  }
+  if (day==4){
+      printf("thrsday");
+  }
+  if (day==5){
+      printf("friday");
+  }
+  if (day==6){
+      printf("saturday");
+  }
+  if (day==7){
+      printf("sunday");
+  }
+  return 0;
+  }
+  
