8428c896c359dba7735d3ed904937b6070a4e52b
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-14 18:14:57 +0530

    Update and rename C32.c to 51.circumference_of_circle.c

---
+++ 51.circumference_of_circle.c
@@ -0,0 +1,9 @@
+int main()
+{
+  printf("RA2211042010036\n");
+  float Pi=3.14, circumference, radius;
+  printf("Enter radius of circle: ");
+  scanf("%f",&radius);
+  circumference = 2*Pi*radius;
+  printf("Circumference of the circle is : %f\n", circumference);
+}
