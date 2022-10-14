a6e551bbc112602864b16b38b0f340ca9051e381
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-14 18:00:16 +0530

    Update and rename C26.c to 49.case_changer.c

---
+++ 49.case_changer.c
@@ -0,0 +1,16 @@
+int main()
+{
+   printf("RA2211042010036\n");
+   char str[25];
+   int i;
+
+   printf("Enter the string:");
+   scanf("%s",str);
+
+   for(i=0;i<=strlen(str);i++){
+      if(str[i]>=65&&str[i]<=90)
+         str[i]=str[i]+32;
+   }
+   printf("\nLower Case String is: %s",str);
+   return 0;
+   }
