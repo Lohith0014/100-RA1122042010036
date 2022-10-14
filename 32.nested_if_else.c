bf98d228035db1354062adb4163c5df65a472303
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-14 17:01:15 +0530

    Update and rename C18.c to 32.nested_if_else.c

--- 32.nested_if_else.c
+++ 32.nested_if_else.c
@@ -1,17 +1,15 @@
 int main()
 {
     printf("RA2211042010036\n");
-    double num;
+    int num;
     printf("Enter a number: ");
-    scanf("%lf", &num);
-    if (num <= 0.0) {
-        if (num == 0.0)
+    scanf("%d", &num);
+    if (num <= 0) {
+        if (num == 0)
             printf("You entered 0.");
         else
             printf("You entered a negative number.");
     } 
     else
         printf("You entered a positive number.");
-
-    return 0;
 }
