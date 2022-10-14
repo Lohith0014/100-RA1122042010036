1ee720942c89e26a1d93af97c0bd7828a6c63cfa
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-14 16:46:06 +0530

    Update and rename C16.c to if_else_comarision.c

--- C16.c
+++
@@ -1,8 +0,0 @@
-int main()
-{
-  printf("RA2211042010036\n");
-  if (20 > 18) {
-    printf("20 is greater than 18");
-  }  
-  return 0;
-}
---
+++ if_else_comarision.c
@@ -0,0 +1,11 @@
+int main()
+{
+  printf("RA2211042010036\n");
+  if (100 > 50) {
+    printf("100 is greater than 50");
+  }  
+  else {
+    printf("50 is greater than 100");
+  }  
+  return 0;
+}
