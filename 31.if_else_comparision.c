51bcb9540b15d841df871089c856f40be498a915
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-14 16:57:04 +0530

    Update and rename C17.c to 31.if_else_comparision.c

---
+++ 31.if_else_comparision.c
@@ -0,0 +1,13 @@
+int main() {
+  printf("RA2211042010036\n");
+  int n1, n2, n3;
+  printf("Enter three different numbers: ");
+  scanf("%d %d %d", &n1, &n2, &n3);
+  if (n1 >= n2 && n1 >= n3)
+    printf("%d is the largest number.", n1);
+  if (n2 >= n1 && n2 >= n3)
+    printf("%d is the largest number.", n2);
+  if (n3 >= n1 && n3 >= n2)
+    printf("%d is the largest number.", n3);
+    
+}
