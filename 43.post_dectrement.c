1f3ad3e145360e61f6a8a31e2de0bff8f12669e7
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-14 17:32:15 +0530

    Update and rename C34.c to 43.post_dectrement.c

---
+++ 43.post_dectrement.c
@@ -0,0 +1,8 @@
+int main() 
+{
+  printf("RA2211042010036\n");
+  int x = 10;
+  printf("%d\n", x--);
+  printf("%d\n", x--);
+  return 0;
+}
--- C34.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-    printf("RA2211042010036\n");
-    double arr[] = {8,18,13,29,7};
-    int n;
-    n = sizeof(arr) / sizeof(arr[0]);
-    printf("Size of the array is: %d\n", n);
-    return 0;
-}
