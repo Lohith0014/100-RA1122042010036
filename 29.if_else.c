401cb5539f985a17009c2f1e7797179a33de76ba
Lohith0014 <113873585+Lohith0014@users.noreply.github.com>
2022-10-14 16:42:57 +0530

    Update and rename C15.c to 29.if_else.c

---
+++ 29.if_else.c
@@ -0,0 +1,15 @@
+int main()
+{
+  printf("RA2211042010036\n");
+  int time = 8;
+  if (time < 10) {
+    printf("Good morning.");
+  } else if (time < 17) {
+    printf("Good afternoon.");
+  } else if (time < 20) {
+    printf("Good evening.");
+  } else {
+    printf("Good night.");
+  }
+  return 0;
+}
